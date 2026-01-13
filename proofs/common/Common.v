7d70d801a654b5fa7fd9bc8b7dbcc9c0e392cfec
Jimmy Situ <web@jimmystone.cn>
2026-01-13 20:41:59 +0800

    Change helper function name for HMap

--- proofs/common/Common.v
+++ proofs/common/Common.v
@@ -1,9 +1,17 @@
 Require Export Pfv.Lib.Common.
 Require Export Pfv.Lib.Lib.
+Require Import Coq.Lists.List.
+Require Import Coq.ZArith.ZArith.
+
+Import ListNotations.
 
 Local Open Scope hmap_scope.
 
-(*
-Definition hsingle_opt (i: vid_t) (v: hmap) : hmap :=
-  if is_empty v then HMapEmpty else HMapStr [(i, v)].
-*)
+Section HMapHelper.
+  Context `{sz_ops}.
+  Context `{vid_ops}.
+
+  Definition hsimple (i: vid_t) (v: hmap) : hmap :=
+    if is_empty v then HMapEmpty else HMapStr [(i, v)].
+
+End HMapHelper.
